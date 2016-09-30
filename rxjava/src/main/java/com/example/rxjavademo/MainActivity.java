d1e8723c87eda7545a889a58077260eb6f954fa8
xujichang <1103713746@qq.com>
2016-09-30 18:57:31 +0800

    Android配置升级为2.2

--- rxjava/src/main/java/com/example/rxjavademo/MainActivity.java
+++ rxjava/src/main/java/com/example/rxjavademo/MainActivity.java
@@ -18,16 +18,20 @@ import java.net.HttpURLConnection;
 import java.net.MalformedURLException;
 import java.net.URL;
 import java.util.ArrayList;
+import java.util.List;
 import java.util.concurrent.TimeUnit;
 
 import rx.Notification;
 import rx.Observable;
+import rx.Scheduler;
 import rx.Subscriber;
 import rx.android.schedulers.AndroidSchedulers;
 import rx.functions.Action0;
 import rx.functions.Action1;
 import rx.functions.Func0;
 import rx.functions.Func1;
+import rx.functions.Func2;
+import rx.functions.FuncN;
 import rx.schedulers.Schedulers;
 import rx.schedulers.TimeInterval;
 import rx.schedulers.Timestamped;
@@ -242,7 +246,7 @@ public class MainActivity extends AppCompatActivity {
 
     /**
      * RXJava amb操作符的使用、
-     * <p/>
+     * <p>
      * 对传入的被观察者，优先取用最先完成任务返回数据的被观察者，其他被观察者则抛弃
      *
      * @return
@@ -259,7 +263,7 @@ public class MainActivity extends AppCompatActivity {
 
     /**
      * RXJava  中all操作符的使用
-     * <p/>
+     * <p>
      * 对输入的数据做统一的比较，当所有的数据都满足给出的条件时 才会返回true
      *
      * @return
@@ -363,7 +367,7 @@ public class MainActivity extends AppCompatActivity {
 
     /**
      * RXJava 中 对接收数据 时机进行控制的操作符
-     * <p/>
+     * <p>
      * skipUntil、skipWhile、takeUntil、takeWhile
      */
     private void takeAndSkipOperator() {
@@ -417,7 +421,7 @@ public class MainActivity extends AppCompatActivity {
 
     /**
      * RXJava中 对事件延迟处理的操作符
-     * <p/>
+     * <p>
      * delay 数据延迟反馈
      * delaySubscription 延时注册观察者
      */
@@ -507,7 +511,7 @@ public class MainActivity extends AppCompatActivity {
 
     /**
      * RXJava 中materialize、dematerialize操作符的使用；
-     * <p/>
+     * <p>
      * materialize将Observable的onNext()、onError()、onComplete() 封装为Notification对象作为返回数据 返回
      * dematerialize 则是相反的操作
      */
@@ -528,7 +532,7 @@ public class MainActivity extends AppCompatActivity {
 
     /**
      * RXJAva  中对于Observable 与 subscriber所在线程的设置
-     * <p/>
+     * <p>
      * subscribeOn 指定被观察者所在的线程，可以理解为对被观察者在那个线程订阅（订阅在XX线程）
      * observeOn 指定观察者所在的线程，即数据的返回后展示或者处理的线程 （我在XX线程接收数据）
      */
@@ -572,7 +576,7 @@ public class MainActivity extends AppCompatActivity {
      * RXJava   中对时间的监听
      * timeInterval 对原来的数据 进行包装 加入时间，来标识与上一次数据返回时的间隔时间
      * timestamp  对原来的数据 进行包装 加入时间戳，来标识数据返回时的时刻
-     * <p/>
+     * <p>
      * timeout 设置超时时间
      */
     private void TimeOperator() {
@@ -606,7 +610,7 @@ public class MainActivity extends AppCompatActivity {
 
     /**
      * RXJava using 操作符的使用
-     * <p/>
+     * <p>
      * 使用给定的资源，并根据资源的变量来动态创建Observable
      *
      * @see Observable#using(Func0, Func1, Action1)
@@ -653,12 +657,19 @@ public class MainActivity extends AppCompatActivity {
 
     /**
      * RXJava 中对Exception的捕获与处理
-     * <p/>
-     *
-     * @see Observable#onErrorReturn(Func1) Error发生时,返回预定义好的数据
-     * @see Observable#onErrorResumeNext(Func1) Error发生时，可根据Throwable，来判断需要使用的Observable
-     * @see Observable#onErrorResumeNext(Observable) Error发生时，使用另一个Observable继续处理数据、
-     * @see Observable#onExceptionResumeNext(Observable) 如果是Exception,则会使用指定的Observable进行数据的继续处理
+     * <p>
+     * <p>
+     * onErrorReturn {@see Observable#onErrorReturn(Func1) }
+     * Error发生时,返回预定义好的数据
+     * <p>
+     * onErrorResumeNext{@see Observable#onErrorResumeNext(Func1)}
+     * Error发生时，可根据Throwable，来判断需要使用的Observable
+     * <p>
+     * onErrorResumeNext{@see Observable#onErrorResumeNext(Observable) }
+     * Error发生时，使用另一个Observable继续处理数据
+     * <p>
+     * onExceptionResumeNext{@see Observable#onExceptionResumeNext(Observable)}
+     * 如果是Exception,则会使用指定的Observable进行数据的继续处理
      * 否则 会将错误分发至Subscribe
      */
     private void errorHandingOperator() {
@@ -668,26 +679,163 @@ public class MainActivity extends AppCompatActivity {
                     public Integer call(Throwable throwable) {
                         return 100;
                     }
-                }).subscribe(new CustomSubscribe<Integer>());
+                }).subscribe(new CustomSubscribe<Integer>("onErrorReturn"));
         createExceptionObservable()
                 .onErrorResumeNext(new Func1<Throwable, Observable<? extends Integer>>() {
                     @Override
                     public Observable<? extends Integer> call(Throwable throwable) {
                         return Observable.just(100, 101, 102);
                     }
-                }).subscribe(new CustomSubscribe<Integer>());
+                }).subscribe(new CustomSubscribe<Integer>("onErrorResumeNext"));
 
         createExceptionObservable()
                 .onExceptionResumeNext(Observable.just(200, 201, 202))
-                .subscribe(new CustomSubscribe<Integer>());
+                .subscribe(new CustomSubscribe<Integer>("onExceptionResumeNext"));
 
     }
+
+    /**
+     * RXJava 中发生错误时，重新建立订阅关系  操作符的使用
+     * <p>
+     * retry{
+     *
+     * @see Observable#retry() 会在每次出现错误时，都重新订阅，会出现无限循环的情况
+     * @see Observable#retry(Func2)  Func2 可以根据错误类型 与重复次数 来控制是否不再重新订阅 而是将错误返回给观察者
+     * @see Observable#retry(long) long 尝试的次数 如果仍然有错，则会将错误返回给观察者
+     * }
+     * retryWhen{
+     * @see Observable#retryWhen(Func1) Func1 返回一个创建好的Observable 当有数据返回时便重新订阅
+     * @see Observable#retryWhen(Func1, Scheduler) 指定作为判断节点的Observable运行在那个线程
+     * }
+     */
+    private void retryOperator() {
+        createExceptionObservable().retry(new Func2<Integer, Throwable, Boolean>() {
+            @Override
+            public Boolean call(Integer integer, Throwable throwable) {
+                return true;
+            }
+        }).subscribe(new CustomSubscribe<Integer>("retry-with_func2"));
+        createExceptionObservable().retryWhen(new Func1<Observable<? extends Throwable>, Observable<Long>>() {
+            @Override
+            public Observable<Long> call(Observable<? extends Throwable> observable) {
+                return Observable.timer(2000, TimeUnit.MILLISECONDS);
+            }
+        }).subscribe(new CustomSubscribe<Integer>("retryWhen-no-scheduler"));
+        createExceptionObservable().retryWhen(new Func1<Observable<? extends Throwable>, Observable<Long>>() {
+            @Override
+            public Observable<Long> call(Observable<? extends Throwable> observable) {
+                return Observable.timer(2000, TimeUnit.MILLISECONDS);
+            }
+        }, Schedulers.immediate()).subscribe(new CustomSubscribe<Integer>("retryWhen-scheduler"));
+    }
+
+    /**
+     * RXJava combineLatest的用法
+     * <p>
+     * 使用list传值
+     *
+     * @see Observable#combineLatest(Iterable, FuncN)
+     * @see Observable#combineLatest(List, FuncN)
+     * <p>
+     * 直接将Observable作为参数
+     * @see Observable#combineLatest(Observable, Observable, Func2)
+     * 错误将在 操作结束后处理
+     * @see Observable#combineLatestDelayError(Iterable, FuncN)
+     */
+    private void CombineLatestOperator() {
+        Observable.combineLatest(createObservable(1), createObservable(2), new Func2<String, String, String>() {
+            @Override
+            public String call(String s, String s2) {
+                return null;
+            }
+        }).subscribe(new CustomSubscribe<String>("combineLatest-Observable"));
+        ArrayList<Observable<String>> mObservables = new ArrayList<Observable<String>>() {
+            {
+                for (int i = 10; i < 25; i++) {
+                    add(createObservable(i));
+                }
+            }
+        };
+        Observable.combineLatest(mObservables, new FuncN<String>() {
+            @Override
+            public String call(Object... args) {
+                String result = "";
+                for (Object mArg : args) {
+                    result = result + " " + String.valueOf(mArg);
+                }
+                return result;
+            }
+        }).subscribe(new CustomSubscribe<String>("combineLatest-List"));
+    }
+
+    /**
+     * RXJava  中join相关操作符的使用
+     *
+     * @see Observable#join(Observable, Func1, Func1, Func2)
+     * @see Observable#groupJoin(Observable, Func1, Func1, Func2)
+     * <p>
+     * Observable 目标Observable
+     * Func1 接收源Observable的数据，并返回一个Observable
+     * Func1  接收目标Observable的数据，并返回一个Observable
+     * Func2 处理源Observable与目标Observable的数据，并返回组合的数据
+     */
+    private void joinOperator() {
+        Observable.just("left-").join(createObservable("right-"), new Func1<String, Observable<Long>>() {
+            @Override
+            public Observable<Long> call(String s) {
+                return Observable.timer(2, TimeUnit.SECONDS);
+            }
+        }, new Func1<String, Observable<Long>>() {
+            @Override
+            public Observable<Long> call(String s) {
+                return Observable.timer(3, TimeUnit.SECONDS);
+            }
+        }, new Func2<String, String, String>() {
+            @Override
+            public String call(String s, String s2) {
+                return s + "  " + s2;
+            }
+        }).subscribe(new CustomSubscribe<String>("join-"));
+
+        Observable.just("left-").groupJoin(createObservable("right-"), new Func1<String, Observable<Long>>() {
+            @Override
+            public Observable<Long> call(String s) {
+                return Observable.timer(2, TimeUnit.SECONDS);
+            }
+        }, new Func1<String, Observable<Long>>() {
+            @Override
+            public Observable<Long> call(String s) {
+                return Observable.timer(3, TimeUnit.SECONDS);
+            }
+        }, new Func2<String, Observable<String>, Observable<String>>() {
+            @Override
+            public Observable<String> call(final String s, Observable<String> stringObservable) {
+                return stringObservable.map(new Func1<String, String>() {
+                    @Override
+                    public String call(String str) {
+                        return s + str;
+                    }
+                });
+            }
+        }).subscribe(new Action1<Observable<String>>() {
+            @Override
+            public void call(Observable<String> stringObservable) {
+                stringObservable.subscribe(new CustomSubscribe<String>());
+            }
+        });
+    }
+
 
     private void setMessage(String str) {
         message.setVisibility(View.VISIBLE);
         message.setText(str);
     }
 
+    /**
+     * 创建一个有延迟效果的被观察者
+     *
+     * @return
+     */
     private Observable<Integer> createNewThreadRunnable() {
         return Observable.create(new Observable.OnSubscribe<Integer>() {
             @Override
@@ -704,6 +852,11 @@ public class MainActivity extends AppCompatActivity {
         }).subscribeOn(Schedulers.newThread());
     }
 
+    /**
+     * 创建一个会报错的被观察者
+     *
+     * @return
+     */
     private Observable<Integer> createExceptionObservable() {
         return Observable.create(new Observable.OnSubscribe<Integer>() {
             @Override
@@ -720,6 +873,11 @@ public class MainActivity extends AppCompatActivity {
         }).subscribeOn(Schedulers.newThread());
     }
 
+    /**
+     * 创建一个会逐步增大睡眠时间，导致超时的被观察者，Observable
+     *
+     * @return
+     */
     private Observable<Integer> createTimeOutObservable() {
         return Observable.create(new Observable.OnSubscribe<Integer>() {
             @Override
@@ -736,15 +894,55 @@ public class MainActivity extends AppCompatActivity {
         }).subscribeOn(Schedulers.newThread());
     }
 
+    private Observable<String> createObservable(final int index) {
+        return Observable.create(new Observable.OnSubscribe<String>() {
+            @Override
+            public void call(Subscriber<? super String> subscriber) {
+                for (int i = 0; i < 10; i++) {
+                    subscriber.onNext("index:" + index + "  i:" + i);
+                    try {
+                        Thread.sleep(1000);
+                    } catch (InterruptedException e) {
+                        e.printStackTrace();
+                    }
+                }
+            }
+        }).subscribeOn(Schedulers.newThread());
+    }
 
+    private Observable<String> createObservable(final String name) {
+        return Observable.create(new Observable.OnSubscribe<String>() {
+            @Override
+            public void call(Subscriber<? super String> subscriber) {
+                for (int i = 0; i < 10; i++) {
+                    subscriber.onNext(name + "  i:" + i);
+                    try {
+                        Thread.sleep(1000);
+                    } catch (InterruptedException e) {
+                        e.printStackTrace();
+                    }
+                }
+            }
+        }).subscribeOn(Schedulers.newThread());
+    }
+
+    /**
+     * @return
+     */
     private Observable<Notification<Integer>> materializeObservable() {
         return Observable.just(1, 2, 3, 4, 5, 6).materialize();
     }
 
+    /**
+     * @return
+     */
     private Observable<Integer> deMaterializeObservable() {
         return materializeObservable().dematerialize();
     }
 
+    /**
+     *
+     */
     private class InnerObject {
 
         CustomSubscribe<Long> mSubscribe = new CustomSubscribe<Long>() {
@@ -764,6 +962,14 @@ public class MainActivity extends AppCompatActivity {
     }
 
     private class CustomSubscribe<T> extends Subscriber<T> {
+        private String TAG = "default";
+
+        public CustomSubscribe(String TAG) {
+            this.TAG = TAG;
+        }
+
+        public CustomSubscribe() {
+        }
 
         @Override
         public void onCompleted() {
